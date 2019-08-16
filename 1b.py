0301583803c434b3faf1ca8df84b8ef93e0f379d
abhay2508 <54144891+abhay2508@users.noreply.github.com>
2019-08-16 10:09:38 +0530

    Add files via upload

---
+++ 1b.py
@@ -0,0 +1,15 @@
+n=int(input("enter the length of the list"))
+x[]
+print("enter the elements in list")
+for i in range(n):
+  a=int(input())
+  x.append(a)
+  
+y[]
+for j in x:
+  if j%2==0
+  y.append(j)
+  
+for k in y:
+  print(k)
+  
